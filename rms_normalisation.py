4002c9ba52db8af1b48923e493bb47dae6c169a0
Melissa <c2032014@newcastle.ac.uk>
2024-09-27 14:23:35 +0100

    calculates rms normalisation

---
+++ rms_normalisation.py
@@ -0,0 +1,20 @@
+import numpy as np
+from stingray import Lightcurve, AveragedCrossspectrum
+
+#calculate the normalisation of fractional frac_rms
+def rms_normalisation(data_12,data_3,bin_length,seg_length,fmin,fmax,GTI):
+        lightcurve_12=Lightcurve.make_lightcurve(data_12['TIME'],dt=bin_length,gti=GTI)
+        lightcurve_12.apply_gtis()
+
+        lc_ref=Lightcurve.make_lightcurve(data_3['TIME'],dt=bin_length,gti=GTI)
+        lc_ref.apply_gtis()
+
+        avg_cs = AveragedCrossspectrum.from_lightcurve(lightcurve_12,lc_ref,seg_length,norm='frac')
+        del lightcurve_12
+
+        av_power_norm_real=avg_cs.power.real[(fmin<=avg_cs.freq) & (avg_cs.freq<=fmax)].mean()
+        norm_factor=(np.sqrt((fmax-fmin))/np.sqrt(av_power_norm_real))
+        del avg_cs
+
+        return norm_factor
+
