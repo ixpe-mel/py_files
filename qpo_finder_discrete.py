21f1e1615f999ef51e33b31f5c097c09969e51b3
Melissa <c2032014@newcastle.ac.uk>
2024-10-02 17:02:55 +0100

    now exports errors

--- qpo_finder_discrete.py
+++ qpo_finder_discrete.py
@@ -1,12 +1,13 @@
 #Discretised version of QPO finder
 import sys
-sys.path.append('/home/c2032014/PhD/py_files/')
+sys.path.append('/home/c2032014/py_files/')
 import load_and_clean as lac
-import py_files.rms_phase_calculator_NEW as rpc
+import rms_phase_calculator_NEW as rpc
 import rms_normalisation as rn
 import numpy as np
+import Ingram_2019_errors as I_19errs
 
-def qpo_finder(file1,file2,output_file,gti,bin_length,seg_length,fmin,fmax,Pmin,Pmax,mod_bin_number):
+def qpo_finder(file1,file2,output_file,gti,bin_length,seg_length,fmin,fmax,Pmin,Pmax,mod_bin_number,coherence_corrector=True):
 
     #load data and clean
     data_1,header_1=lac.load_and_clean(file1,Pmin,Pmax)
@@ -17,5 +18,21 @@ def qpo_finder(file1,file2,output_file,gti,bin_length,seg_length,fmin,fmax,Pmin,
 
     #Calculate G, subtracting spurious polarisation
 
-    frac_rms,phase=rpc.rms_phase_calc(data_1,lc_ref,bin_length,seg_length,fmin,fmax,norm_factor,mod_bin_number)
+    frac_rms,frac_rms_err,phase, phase_err=rpc.rms_phase_anderr_calc(data_1,data_2,gti,lc_ref,bin_length,seg_length,fmin,fmax,norm_factor,mod_bin_number,coherence_corrector)
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     return frac_rms,phase
+
