0e22ab5a4b48edad5c76f0b6f40b25dfb27ff93b
Melissa <c2032014@newcastle.ac.uk>
2024-10-02 17:00:29 +0100

    now exports all fit file hdus

--- load_and_clean.py
+++ load_and_clean.py
@@ -3,8 +3,12 @@ from astropy.io import fits
 def load_and_clean(file, Pmin, Pmax):
     with fits.open(str(file)) as hdu:
                 data=hdu[1].data
+                GTI=hdu[2].data
+                GTI_header=hdu[2].header
                 header=hdu[1].header
+                prihdr = hdu[0].header
+                prihdu = fits.PrimaryHDU(header=prihdr)
 
     data=data[(Pmin<=data['PI']) & (data['PI']<=Pmax)]
     data=data[data['QUAL']==1]
-    return data,header
+    return data,header,GTI,GTI_header,prihdu
