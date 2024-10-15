ba6144e5e260814f22b9c8d811eae54911059b5b
Melissa <c2032014@newcastle.ac.uk>
2024-10-15 17:12:54 +0100

    playing with more statistically robust dG calculation

--- Ingram_2019_errors.py
+++ Ingram_2019_errors.py
@@ -5,10 +5,10 @@ import importlib
 importlib.reload(lcss)
 from stingray import Lightcurve, Crossspectrum, AveragedCrossspectrum, Powerspectrum
 #Calculating Ingram 2019 Errorbars on rms and phase
-def ingr_2019_errs_cc(mod_min,mod_max,mod_bin_number,real_G,im_G,lc_subject,cs_ref_real_mean,data_2,ps_2_mean,n,m,fmin,fmax,seg_length,bin_length,GTI):
+def ingr_2019_errs_cc(mod_min,mod_max,mod_bin_number,real_G,im_G,lc_subject,cs_ref_real_mean,data_2,ps_2_mean,n,m,fmin,fmax,seg_length,bin_length,GTI,spurious_sub):
    
         # Creating Cross spectrum 'ith'
-
+    #print(GTI)
     av_mod=(mod_min+mod_max)/2
 
 
@@ -16,27 +16,31 @@ def ingr_2019_errs_cc(mod_min,mod_max,mod_bin_number,real_G,im_G,lc_subject,cs_r
     lc2_subject=Lightcurve.make_lightcurve(data_cut_2['TIME'],dt=bin_length,gti=GTI)
     lc2_subject.apply_gtis()
     lc2_subject_countrate2=lc2_subject.meanrate
-    if spur_sub=True
+    if spurious_sub==True:
+        print('Spurious sub on lc2')
         lc2_subject=lcss.lc_spur_sub(data_cut_2,lc2_subject_countrate2,mod_bin_number,av_mod,bin_length,lc2_subject)
     #lc2_subject_countrate2=lc2_subject.meanrate
     else:
         lc2_subject=lc2_subject 
-
+    print('lc subject type',type(lc_subject))
     ps_1_subject=Powerspectrum.from_lightcurve(lc_subject,seg_length,norm='frac')
     ps_1_subject_av=ps_1_subject.power[(fmin<=ps_1_subject.freq) & (ps_1_subject.freq<=fmax)].mean()
+    print('ps_1_subject_av',ps_1_subject_av)
     del ps_1_subject
 
 
     cs_ith=AveragedCrossspectrum.from_lightcurve(lc_subject,lc2_subject,seg_length,norm='frac')
     cs_ith_real_mean=cs_ith.power.real[(fmin<=cs_ith.freq) & (cs_ith.freq<=fmax)].mean()
-    cs_ith_im_mean=cs_ith.power.imag[(fmin<=cs_ith.freq) & (cs_ith.freq<=fmax)].mean()
+    print('cs_ith_real_mean',cs_ith_real_mean)
+    #cs_ith_im_mean=cs_ith.power.imag[(fmin<=cs_ith.freq) & (cs_ith.freq<=fmax)].mean()
     del cs_ith
-
-    
-    # Power spectrum from file 2
     modulus_G=np.sqrt((real_G**2)+(im_G**2))
+    cs_ith_new=modulus_G**2/(cs_ref_real_mean)
+    print('cs_ith_new',cs_ith_new)
+    # Power spectrum from file 2
+    
 
-    #print(modulus_G)
+    print('modulus G',modulus_G)
     #Catching if the coherence is below 0 or above 1 and correcting....
     #print('modulus_G',modulus_G)
     #print('cs_ref_real_mean',cs_ref_real_mean)
@@ -44,15 +48,27 @@ def ingr_2019_errs_cc(mod_min,mod_max,mod_bin_number,real_G,im_G,lc_subject,cs_r
     coherence=modulus_G**2/(cs_ref_real_mean*cs_ith_real_mean)
     print('coherence',coherence)
 
-    new_coherence = np.clip(coherence, 0, 1)
+    corrected_coherence = np.clip(coherence, 0, 1)
     #print('ps_2_mean',ps_2_mean)
     #print('cs_ith_real_mean',cs_ith_real_mean)
     #print('new_coherence',new_coherence)
     #print(ps_1_subject_av)
     #print('new_coherence',new_coherence)
-    dG=np.sqrt(      (ps_2_mean / (2*n*m)) * (ps_1_subject_av - new_coherence*(cs_ith_real_mean) )     )
-       
-    return dG
+    #dG_2=
+
+    dG_original=np.sqrt(      (ps_2_mean / (2*n*m)) * (ps_1_subject_av - corrected_coherence*(cs_ith_real_mean) )     )
+    print('dG_original',dG_original)
+    dG_new_csith=np.sqrt(      (ps_2_mean / (2*n*m)) * (ps_1_subject_av - corrected_coherence*(cs_ith_new) ) )
+    print('dG_new_csith',dG_new_csith)
+    dG_nogamma=np.sqrt(    (ps_2_mean/(2*n*m))   *   (ps_1_subject_av - (modulus_G**2/cs_ref_real_mean)    ))    
+    print('dG_nogamma',dG_nogamma)
+
+    dG_adam=np.sqrt( (  (ps_2_mean*ps_1_subject_av) - ( (ps_2_mean/cs_ref_real_mean) * modulus_G**2   )       )    / (2*n*m)      )
+    print(dG_adam)
+
+
+    
+    return dG_original
 
 
 #Assuming no coherence correction, calculate Ingram 2019 errorbars dG from 
@@ -67,3 +83,5 @@ def ingr_2019_errs(real_G,im_G,lc_subject,cs_ref_real_mean,ps_2_mean,n,m,fmin,fm
         
     dG=np.sqrt(    (ps_2_mean/(2*n*m))   *   (ps_1_subject_av - (modulus_G**2/cs_ref_real_mean)    ))
     return dG
+
+    #if square bracket negative set poisson noise 0 so its just ps1sub-/...
