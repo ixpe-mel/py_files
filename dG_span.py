db580e0807b7078796749e5eab4599067e2fe617
Melissa <c2032014@newcastle.ac.uk>
2024-10-15 17:10:07 +0100

    currently not working lol

--- dG_span.py
+++ dG_span.py
@@ -2,7 +2,9 @@
 import numpy as np
 import dG_calc as dGc
 from functools import partial
-def dG_span(mod_bin_number,G_real,G_im,lc_subject,lc_1_ref,lc_2_ref,data_2,n,m,fmin,fmax,seg_length,bin_length,GTI,coherence_corrector=True):
+import importlib
+importlib.reload(dGc)
+def dG_span(mod_bin_number,G_real,G_im,lc_subject,lc_1_ref,lc_2_ref,data_2,n,m,fmin,fmax,seg_length,bin_length,GTI,coherence_corrector=True,spurious_sub=True):
 
     #create modulation angle bins
     mod_min_global = np.radians(-90)
@@ -14,12 +16,21 @@ def dG_span(mod_bin_number,G_real,G_im,lc_subject,lc_1_ref,lc_2_ref,data_2,n,m,f
     av_mod = (mod_min_array + mod_max_array) / 2
     av_mod_err = (mod_max_array - mod_min_array) / 2
 
-    if coherence_corrector==True:
+    if coherence_corrector==True and spurious_sub==True:
+        print('Applying coherence correction and spurious sub...')
         partial_dG_calc=partial(dGc.dG_calc, G_real=G_real,
-               G_im=G_im,lc_subject=lc_subject,lc_1_ref=lc_1_ref,lc_2_ref=lc_2_ref,data_2=data_2,n=n,m=m,fmin=fmin,fmax=fmax,seg_length=seg_length,bin_length=bin_length,mod_bin_number=mod_bin_number,GTI=GTI)
-    else:
+               G_im=G_im,lc_1_ref=lc_1_ref,lc_2_ref=lc_2_ref,data_2=data_2,n=n,m=m,fmin=fmin,fmax=fmax,seg_length=seg_length,bin_length=bin_length,mod_bin_number=mod_bin_number,GTI=GTI)
+        
+    elif coherence_corrector==True and spurious_sub==False:
+        print('Applying coherence correction BUT NO SPUR SUB...')
         partial_dG_calc=partial(dGc.dG_calc, G_real=G_real,
-               G_im=G_im,lc_subject=lc_subject,lc_1_ref=lc_1_ref,lc_2_ref=lc_2_ref,data_2=data_2,n=n,m=m,fmin=fmin,fmax=fmax,seg_length=seg_length,bin_length=bin_length,GTI=GTI,coherence_corrector=False)
+               G_im=G_im,lc_1_ref=lc_1_ref,lc_2_ref=lc_2_ref,data_2=data_2,n=n,m=m,fmin=fmin,fmax=fmax,seg_length=seg_length,bin_length=bin_length,GTI=GTI,coherence_corrector=False)
+    elif coherence_corrector==False and spurious_sub==True:
+        partial_dG_calc=partial(dGc.dG_calc, G_real=G_real,
+               G_im=G_im,lc_1_ref=lc_1_ref,lc_2_ref=lc_2_ref,data_2=data_2,n=n,m=m,fmin=fmin,fmax=fmax,seg_length=seg_length,bin_length=bin_length,mod_bin_number=mod_bin_number,GTI=GTI,spurious_sub=False)
+    else:    
+        partial_dG_calc=partial(dGc.dG_calc, G_real=G_real,
+               G_im=G_im,lc_1_ref=lc_1_ref,lc_2_ref=lc_2_ref,data_2=data_2,n=n,m=m,fmin=fmin,fmax=fmax,seg_length=seg_length,bin_length=bin_length,GTI=GTI,coherence_corrector=False,spurious_sub=False)
     #print('mod_min_array',mod_min_array)
 
     #print('mod_max_array',mod_max_array)
@@ -27,8 +38,8 @@ def dG_span(mod_bin_number,G_real,G_im,lc_subject,lc_1_ref,lc_2_ref,data_2,n,m,f
     
     #vectorize real/im calculator
     partial_dG_calc_VEC = np.vectorize(partial_dG_calc,otypes=[object])
-
+    print('moving to g calc')
     # Get the real and imaginary parts of the power spectrum for the current modulation range
-    result= partial_dG_calc_VEC(mod_min_array, mod_max_array)#,data_1, lc_ref,GTI,bin_length, seg_length, fmin, fmax,spur_sub)
+    result= partial_dG_calc_VEC(mod_min_array, mod_max_array,lc_subject)#,data_1, lc_ref,GTI,bin_length, seg_length, fmin, fmax,spur_sub)
     # = zip(*result)
     return result
