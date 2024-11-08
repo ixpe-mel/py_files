62db37f1236d87d25cc79a4720259ad9f983f12b
Melissa <c2032014@newcastle.ac.uk>
2024-11-08 14:47:11 +0000

    fixing bugs

--- fit_rms_phase.py
+++ fit_rms_phase.py
@@ -13,21 +13,25 @@ def fit_line(av_mod,frac_rms,frac_rms_err,phase,phase_err):
     
     #Fitting line to rms
     parameters_line_fracrms, covariance_fracrms = curve_fit(fm.line, av_mod ,frac_rms,maxfev=50000) #defining the returns of curvefit
-    fit_y_line_fracrms = fm.line(np.array(av_mod),fit_const_fracrms)
+    fit_y_line_fracrms = fm.line(np.array(av_mod),parameters_line_fracrms)
     #print('frac rms const fit {}'.format(fit_const_fracrms))
     
-    
+    print('av_mod:',av_mod)
+    print(np.array(av_mod))
+    print(len(av_mod))
+    print(len(np.array(av_mod)))
     #Fitting line to phase
-    parameters_line_phase, covariance_phase = curve_fit(fm.line, av_mod ,phase,maxfev=50000) #defining the returns of curvefit
-    fit_y_line_phase = fm.line(np.array(av_mod),fit_const_phase)
+    parameters_line_phase, covariance_phase = curve_fit(fm.line, np.array(av_mod) ,phase,maxfev=50000) #defining the returns of curvefit
+    fit_y_line_phase = fm.line(np.array(av_mod),parameters_line_phase)
     
-    dof_line_rms,dof_line_phase=len(av_mod)-1
+    dof_line_rms=len(av_mod)-1
+    dof_line_phase=len(av_mod)-1
 
 
     frac_rms_line_chi=cs.chi_square(frac_rms,fit_y_line_fracrms,frac_rms_err)#phase,phase_err,fit_y_line_phase,dof_line)
     phase_line_chi=cs.chi_square(phase,phase_err,fit_y_line_phase,dof_line_rms,dof_line_phase)
 
-    reduced_chi_line=cs.reduced_chi_square(frac_rms_line_chi,phase_line_chi,dof_line)
+    reduced_chi_line=cs.reduced_chi_square(frac_rms_line_chi,phase_line_chi,dof_line_rms,dof_line_phase)
     return parameters_line_fracrms,fit_y_line_fracrms,parameters_line_phase,fit_y_line_phase,dof_line,frac_rms_line_chi,phase_line_chi,reduced_chi_line
 
 def fit_sine_90(av_mod,frac_rms,frac_rms_err,phase,phase_err):
@@ -47,9 +51,9 @@ def fit_sine_90(av_mod,frac_rms,frac_rms_err,phase,phase_err):
     dof_sin90_phase=len(av_mod)-3
 
 
-    frac_rms_sin90_chi=cs.chi_square(frac_rms,fit_y_sin90_fracrms,frac_rms_err)#phase,phase_err,fit_y_line_phase,dof_line)
-    phase_sin90_chi=cs.chi_square(phase,phase_err,fit_y_sin90_phase,dof_line)
-    reduced_chi_sin90=cs.reduced_chi_square(frac_rms_sin90_chi,phase_sin90_chi,dof_line)
+    frac_rms_sin90_chi=cs.chi_square(frac_rms,fit_y_sin90_rms,frac_rms_err)#phase,phase_err,fit_y_line_phase,dof_line)
+    phase_sin90_chi=cs.chi_square(phase,phase_err,fit_y_sin90_phase)
+    reduced_chi_sin90=cs.reduced_chi_square(frac_rms_sin90_chi,phase_sin90_chi,dof_sin90_rms,dof_sin90_phase)
     return parameters_sin90_fracrms,fit_y_sin90_fracrms,parameters_sin90_phase,fit_y_sin90_phase,dof_sin90_rms,dof_sin90_phase,frac_rms_sin90_chi,phase_sin90_chi,reduced_chi_sin90
 
 def fit_sine_180(av_mod,frac_rms,frac_rms_err,phase,phase_err):
@@ -61,8 +65,8 @@ def fit_sine_180(av_mod,frac_rms,frac_rms_err,phase,phase_err):
     parameters_sin180_phase, sin180_covariance_phase = curve_fit(fm.sin_180, av_mod, phase,maxfev=50000)#,p0=im_sin_initial_guess)
 
 
-    fit_y_sin180_rms = fm.sin_180(av_mod, fit_A_0_180_rms, fit_A_180_rms,fit_delta_180_rms)
-    fit_y_sin180_phase=fm.sin_180(av_mod,fit_A_0_180_phase,fit_A_180_phase,fit_delta_180_phase)
+    fit_y_sin180_rms = fm.sin_180(av_mod, parameters_sin180_fracrms[0], parameters_sin180_fracrms[1],parameters_sin180_fracrms[2])
+    fit_y_sin180_phase=fm.sin_180(av_mod,parameters_sin180_phase[0],parameters_sin180_phase[1],parameters_sin180_phase[2])
 
 
     #Calculating chi sqr value of 90 sine fit
@@ -70,8 +74,8 @@ def fit_sine_180(av_mod,frac_rms,frac_rms_err,phase,phase_err):
     dof_sin180_phase=len(av_mod)-3
 
 
-    frac_rms_sin180_chi=cs.chi_square(frac_rms,fit_y_sin180_fracrms,frac_rms_err)#phase,phase_err,fit_y_line_phase,dof_line)
-    phase_sin180_chi=cs.chi_square(phase,phase_err,fit_y_sin180_phase,dof_line)
+    frac_rms_sin180_chi=cs.chi_square(frac_rms,fit_y_sin180_rms,frac_rms_err)#phase,phase_err,fit_y_line_phase,dof_line)
+    phase_sin180_chi=cs.chi_square(phase,phase_err,fit_y_sin180_phase,dof_sin180_rms,dof_sin180_phase)
     reduced_chi_sin180=cs.reduced_chi_square(frac_rms_sin180_chi,phase_sin180_chi,dof_sin180_rms,dof_sin180_phase)
     return parameters_sin180_fracrms,fit_y_sin180_fracrms,parameters_sin180_phase,fit_y_sin180_phase,dof_sin180_rms,dof_sin180_phase,frac_rms_sin180_chi,phase_sin180_chi,reduced_chi_sin180
 
@@ -90,12 +94,12 @@ def fit_sinsum(av_mod,frac_rms,frac_rms_err,phase,phase_err):
 
 
     #Calculating chi sqr value of 90 sine fit
-    dof_sinsum_rms=len(av_mod)-3
-    dof_sinsum_phase=len(av_mod)-3
+    dof_sinsum_rms=len(av_mod)-5
+    dof_sinsum_phase=len(av_mod)-5
 
 
-    frac_rms_sinsum_chi=cs.chi_square(frac_rms,fit_y_sinsum_fracrms,frac_rms_err)#phase,phase_err,fit_y_line_phase,dof_line)
-    phase_sinsum_chi=cs.chi_square(phase,phase_err,fit_y_sinsum_phase,dof_line)
+    frac_rms_sinsum_chi=cs.chi_square(frac_rms,fit_y_sinsum_rms,frac_rms_err)#phase,phase_err,fit_y_line_phase,dof_line)
+    phase_sinsum_chi=cs.chi_square(phase,phase_err,fit_y_sinsum_phase,dof_sinsum_rms,dof_sinsum_phase)
     reduced_chi_sinsum=cs.reduced_chi_square(frac_rms_sinsum_chi,phase_sinsum_chi,dof_sinsum_rms,dof_sinsum_phase)
     return parameters_sinsum_fracrms,fit_y_sinsum_fracrms,parameters_sinsum_phase,fit_y_sinsum_phase,dof_sinsum_rms,dof_sinsum_phase,frac_rms_sinsum_chi,phase_sinsum_chi,reduced_chi_sinsum
 
