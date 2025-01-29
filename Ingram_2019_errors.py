3585e65e5ceecf5ffcd3923294226b1921a6d4a0
Melissa <c2032014@newcastle.ac.uk>
2025-01-29 17:22:42 +0000

    allows norm change

--- Ingram_2019_errors.py
+++ Ingram_2019_errors.py
@@ -4,10 +4,10 @@ import importlib
 from stingray import Powerspectrum
 
 #Calculating Ingram 2019 Errorbars on rms and phase
-def ingr_2019_errs_cc(real_G,im_G,lc_subject,cs_ref_real_mean,ps_2_mean,n,m,fmin,fmax,seg_length):
+def ingr_2019_errs_cc(real_G,im_G,lc_subject,cs_ref_real_mean,ps_2_mean,n,m,fmin,fmax,seg_length,norm):
    
 
-    ps_1_subject=Powerspectrum.from_lightcurve(lc_subject,seg_length,norm='frac')
+    ps_1_subject=Powerspectrum.from_lightcurve(lc_subject,seg_length,norm=norm)
     ps_1_subject_av=ps_1_subject.power[(fmin<=ps_1_subject.freq) & (ps_1_subject.freq<=fmax)].mean()
 
     del ps_1_subject
@@ -23,9 +23,10 @@ def ingr_2019_errs_cc(real_G,im_G,lc_subject,cs_ref_real_mean,ps_2_mean,n,m,fmin
     corrected_coherence = np.clip(coherence, 0, 1)
   
     dG=np.sqrt(      (ps_2_mean / (2*n*m)) * (ps_1_subject_av - (corrected_coherence*cs_ith_new) )     )
-    print('Pr3=',ps_2_mean)
-    print('Ps12=',ps_1_subject_av)
-    print('mod G=',modulus_G)
+    #print('Pr3=',ps_2_mean)
+    #print('Ps12=',ps_1_subject_av)
+    #print('mod G=',modulus_G)
+    #print('dG',dG)
 #    print('Pco_r_123=',cs_ref_real_mean)
 
     #print('dG_original',dG_original)
@@ -44,12 +45,12 @@ def ingr_2019_errs_cc(real_G,im_G,lc_subject,cs_ref_real_mean,ps_2_mean,n,m,fmin
  #       print('1st bit',(ps_2_mean*ps_1_subject_av))
  #       print('2nd bit',(modulus_G**2))
         dG=np.sqrt(( (ps_2_mean*ps_1_subject_av) - modulus_G**2)/(2*n*m))
- #       print('dG',dG)
+        #print('dG',dG)
     else:
 #        print('Catch not used')
 #        print('Pco_r_123=',cs_ref_real_mean)
         dG=dG
-    print('dG',dG)
+    #print('dG',dG)
     #print(dG_adam)
 
 
@@ -59,26 +60,28 @@ def ingr_2019_errs_cc(real_G,im_G,lc_subject,cs_ref_real_mean,ps_2_mean,n,m,fmin
 
 #Assuming no coherence correction, calculate Ingram 2019 errorbars dG from 
 
-def ingr_2019_errs(real_G,im_G,lc_subject,cs_ref_real_mean,ps_2_mean,n,m,fmin,fmax,seg_length):
+def ingr_2019_errs(real_G,im_G,lc_subject,cs_ref_real_mean,ps_2_mean,n,m,fmin,fmax,seg_length,norm):
     modulus_G=np.sqrt((real_G**2)+(im_G**2))
     # Creating Powerspectrum
 
-    ps_1_subject=Powerspectrum.from_lightcurve(lc_subject,seg_length,norm='frac')
+    ps_1_subject=Powerspectrum.from_lightcurve(lc_subject,seg_length,norm=norm)
     ps_1_subject_av=ps_1_subject.power[(fmin<=ps_1_subject.freq) & (ps_1_subject.freq<=fmax)].mean()
     del ps_1_subject
         
     dG=np.sqrt(    (ps_2_mean/(2*n*m))   *   (ps_1_subject_av - (modulus_G**2/cs_ref_real_mean)    ))
-    print('Pr3=',ps_2_mean)
-    print('Ps12=',ps_1_subject_av)
-    print('mod G=',modulus_G)
-    print('Pco_r_123=',cs_ref_real_mean)
+    #print('Pr3=',ps_2_mean)
+    #print('Ps12=',ps_1_subject_av)
+    #print('mod G=',modulus_G)
+    #print('Pco_r_123=',cs_ref_real_mean)
     if math.isnan(dG):
-#        print('dG is nan')
+        #print('dG is nan')
         dG=np.sqrt(((ps_2_mean*ps_1_subject_av) - modulus_G**2)/(2*n*m))
     else:
         dG=dG
     
     
-    print('dG=',dG)
+
+
+    #print('dG=',dG)
     
     return dG
