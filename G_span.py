ff632d4f93a48a2dcff3adf3aec5b86d241df0a0
Melissa <c2032014@newcastle.ac.uk>
2025-01-29 17:21:18 +0000

    allows norm change

--- G_span.py
+++ G_span.py
@@ -5,7 +5,7 @@ import numpy as np
 from functools import partial
 import importlib
 importlib.reload(Gc)
-def G_span(mod_bin_number,data_1,lc_ref,GTI,bin_length, seg_length, fmin, fmax,spur_sub):
+def G_span(mod_bin_number,data_1,lc_ref,GTI,bin_length, seg_length, fmin, fmax,spur_sub,norm='frac'):
 
     #create modulation angle bins
     mod_min_global = np.radians(-90)
@@ -19,12 +19,15 @@ def G_span(mod_bin_number,data_1,lc_ref,GTI,bin_length, seg_length, fmin, fmax,s
     #print('mod_min_array',mod_min_array)
     #print('mod_max_array',mod_max_array)
     #Make partial function
-    partial_G_calc = partial(Gc.G_calc, data_1=data_1,lc_ref=lc_ref,GTI=GTI,bin_length=bin_length, seg_length=seg_length, fmin=fmin, fmax=fmax,mod_bin_number=mod_bin_number,spur_sub=spur_sub)
+    partial_G_calc = partial(Gc.G_calc, data_1=data_1,lc_ref=lc_ref,GTI=GTI,bin_length=bin_length, seg_length=seg_length, fmin=fmin, fmax=fmax,mod_bin_number=mod_bin_number,spur_sub=spur_sub,norm=norm)
     
     #vectorize real/im calculator
     partial_G_calc_VEC = np.vectorize(partial_G_calc,otypes=[object])
 
     # Get the real and imaginary parts of the power spectrum for the current modulation range
     result= partial_G_calc_VEC(mod_min_array, mod_max_array)#,data_1, lc_ref,GTI,bin_length, seg_length, fmin, fmax,spur_sub)
-    G_real, G_im, n, m, lc_subject = zip(*result)
-    return G_real, G_im, n, m,lc_subject
+    G_real, G_im, n, m, lc_subject,lc_spur,cs= zip(*result)
+    #cs_err_sr_real,cs_err_sr_im ,cs= zip(*result)
+    return G_real, G_im, n, m,lc_subject,lc_spur,cs
+#,lc_sub
+#,cs_err_sr_real,cs_err_sr_im,cs
