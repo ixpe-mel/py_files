da589c1649613ae9206ca80f24a28da6abe9ff59
Melissa <c2032014@newcastle.ac.uk>
2025-01-29 17:19:34 +0000

    allows norm changes

--- dG_calc.py
+++ dG_calc.py
@@ -4,14 +4,14 @@ import Ingram_2019_errors as I_19errs
 import importlib
 import lc_spurious_sub as lcss
 importlib.reload(I_19errs)
-def dG_calc(lc_subject,G_real,G_im,n,m,fmin,fmax,seg_length,ps_2_mean,cs_ref_real_mean,coherence_corrector):
+def dG_calc(lc_subject,G_real,G_im,n,m,fmin,fmax,seg_length,ps_2_mean,cs_ref_real_mean,coherence_corrector,norm):
 
     if coherence_corrector==True:
-        print('Applying coherence correction')
-        dG=I_19errs.ingr_2019_errs_cc(G_real,G_im,lc_subject,cs_ref_real_mean,ps_2_mean,n,m,fmin,fmax,seg_length)
+        #print('Applying coherence correction')
+        dG=I_19errs.ingr_2019_errs_cc(G_real,G_im,lc_subject,cs_ref_real_mean,ps_2_mean,n,m,fmin,fmax,seg_length,norm)
 
     else:
-        print('Applying no coherence correction...')
-        dG=I_19errs.ingr_2019_errs(real_G=G_real,im_G=G_im,lc_subject=lc_subject,cs_ref_real_mean=cs_ref_real_mean,ps_2_mean=ps_2_mean,n=n,m=m,fmin=fmin,fmax=fmax,seg_length=seg_length)
+        #print('Applying no coherence correction...')
+        dG=I_19errs.ingr_2019_errs(real_G=G_real,im_G=G_im,lc_subject=lc_subject,cs_ref_real_mean=cs_ref_real_mean,ps_2_mean=ps_2_mean,n=n,m=m,fmin=fmin,fmax=fmax,seg_length=seg_length,norm=norm)
 
     return dG
